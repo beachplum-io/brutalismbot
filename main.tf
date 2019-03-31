6727d42e942f8da1ebdd79ea3cd1d49161506e1c
Alexander Mancevice <amancevice@cargometrics.com>
2019-03-30 21:45:47 -0400

    website init

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider aws {
 locals {
   tags {
     App     = "brutalismbot"
+    Name    = "brutalismbot.com"
     Release = "${var.release}"
     Repo    = "${var.repo}"
   }
@@ -36,6 +37,84 @@ data aws_iam_policy_document s3 {
       "${aws_s3_bucket.brutalismbot.arn}/*",
     ]
   }
+}
+
+resource aws_acm_certificate cert {
+  domain_name       = "brutalismbot.com"
+  tags              = "${local.tags}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource aws_acm_certificate_validation cert {
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.cert.fqdn}"]
+}
+
+resource aws_cloudfront_distribution website {
+  aliases             = ["brutalismbot.com", "www.brutalismbot.com"]
+  default_root_object = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_100"
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/error.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${aws_s3_bucket.website.bucket}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  origin {
+    domain_name = "${aws_s3_bucket.website.bucket_regional_domain_name}"
+    origin_id   = "${aws_s3_bucket.website.bucket}"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = "${aws_acm_certificate_validation.cert.certificate_arn}"
+    minimum_protocol_version = "TLSv1.1_2016"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+resource aws_api_gateway_base_path_mapping api {
+  api_id      = "${module.slackbot.api_id}"
+  domain_name = "${aws_api_gateway_domain_name.api.domain_name}"
+  stage_name  = "${module.slackbot.api_stage_name}"
+  base_path   = "slack"
+}
+
+resource aws_api_gateway_domain_name api {
+  certificate_arn = "${aws_acm_certificate_validation.cert.certificate_arn}"
+  domain_name     = "api.brutalismbot.com"
 }
 
 resource aws_cloudwatch_dashboard dash {
@@ -194,6 +273,31 @@ resource aws_lambda_permission uninstall {
   source_arn    = "${aws_sns_topic.uninstall.arn}"
 }
 
+resource aws_route53_zone website {
+  comment = "HostedZone created by Route53 Registrar"
+  name    = "brutalismbot.com"
+}
+
+resource aws_route53_record cert {
+  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 300
+  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${aws_route53_zone.website.id}"
+}
+
+resource aws_route53_record api {
+  name    = "${aws_api_gateway_domain_name.api.domain_name}"
+  type    = "A"
+  zone_id = "${aws_route53_zone.website.id}"
+
+  alias {
+    evaluate_target_health = true
+    name                   = "${aws_api_gateway_domain_name.api.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.api.cloudfront_zone_id}"
+  }
+}
+
 resource aws_s3_bucket brutalismbot {
   acl           = "private"
   bucket        = "brutalismbot"
@@ -207,6 +311,12 @@ resource aws_s3_bucket brutalismbot {
       }
     }
   }*/
+}
+
+resource aws_s3_bucket website {
+  acl           = "public-read"
+  bucket        = "brutalismbot.com"
+  force_destroy = false
 }
 
 resource aws_s3_bucket_notification mirror {
@@ -244,7 +354,7 @@ module slackbot {
   api_name             = "brutalismbot"
   api_stage_name       = "v1"
   api_stage_tags       = "${local.tags}"
-  base_url             = "/brutalismbot"
+  base_url             = "/slack"
   kms_key_id           = "${data.aws_kms_key.key.key_id}"
   lambda_function_name = "brutalismbot-api"
   lambda_tags          = "${local.tags}"
