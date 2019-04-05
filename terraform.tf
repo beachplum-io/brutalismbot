3fcc27310c4556722f299e7213eb35ad29e4fd32
Alexander Mancevice <amancevice@cargometrics.com>
2019-04-05 12:53:28 -0400

    update package name

--- terraform.tf
+++ terraform.tf
@@ -28,7 +28,7 @@ locals {
 }
 
 data archive_file package {
-  output_path = "${path.module}/dist/package.zip"
+  output_path = "${path.module}/dist/brutalismbot-${var.release}.zip"
   source_dir  = "${path.module}/lib"
   type        = "zip"
 }
