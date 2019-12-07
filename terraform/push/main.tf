cf590ba17fea0095eae2eee27694a49d166a27bd
Alexander Mancevice <amancevice@cargometrics.com>
2019-12-07 10:39:18 -0500

    push module

---
+++ terraform/push/main.tf
@@ -0,0 +1,72 @@
+locals {
+  lambda_role                 = var.lambda_role
+  lambda_s3_bucket            = var.lambda_s3_bucket
+  lambda_s3_key               = var.lambda_s3_key
+  posts_s3_bucket             = var.posts_s3_bucket
+  posts_s3_prefix             = var.posts_s3_prefix
+  slack_s3_bucket             = var.slack_s3_bucket
+  slack_s3_prefix             = var.slack_s3_prefix
+  twitter_access_token        = var.twitter_access_token
+  twitter_access_token_secret = var.twitter_access_token_secret
+  twitter_consumer_key        = var.twitter_consumer_key
+  twitter_consumer_secret     = var.twitter_consumer_secret
+  tags                        = var.tags
+}
+
+data aws_iam_role role {
+  name = local.lambda_role
+}
+
+data aws_s3_bucket brutalismbot {
+  bucket = local.posts_s3_bucket
+}
+
+resource aws_cloudwatch_log_group push {
+  name              = "/aws/lambda/${aws_lambda_function.push.function_name}"
+  retention_in_days = 30
+  tags              = local.tags
+}
+
+resource aws_lambda_function push {
+  description   = "Push posts from /r/brutalism"
+  function_name = "brutalismbot-push"
+  handler       = "lambda.push"
+  role          = data.aws_iam_role.role.arn
+  runtime       = "ruby2.5"
+  s3_bucket     = local.lambda_s3_bucket
+  s3_key        = local.lambda_s3_key
+  tags          = local.tags
+  timeout       = 30
+
+  environment {
+    variables = {
+      POSTS_S3_BUCKET             = local.posts_s3_bucket
+      POSTS_S3_PREFIX             = local.posts_s3_prefix
+      SLACK_S3_BUCKET             = local.slack_s3_bucket
+      SLACK_S3_PREFIX             = local.slack_s3_prefix
+      TWITTER_ACCESS_TOKEN        = local.twitter_access_token
+      TWITTER_ACCESS_TOKEN_SECRET = local.twitter_access_token_secret
+      TWITTER_CONSUMER_KEY        = local.twitter_consumer_key
+      TWITTER_CONSUMER_SECRET     = local.twitter_consumer_secret
+    }
+  }
+}
+
+resource aws_lambda_permission push {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.push.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = data.aws_s3_bucket.brutalismbot.arn
+}
+
+resource aws_s3_bucket_notification push {
+  bucket = data.aws_s3_bucket.brutalismbot.id
+
+  lambda_function {
+    id                  = "push"
+    lambda_function_arn = aws_lambda_function.push.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = local.posts_s3_prefix
+    filter_suffix       = ".json"
+  }
+}
---
+++ terraform/push/variables.tf
@@ -0,0 +1,49 @@
+variable lambda_role {
+  description = "Lambda IAM Role name."
+}
+
+variable lambda_s3_bucket {
+  description = "Lambda function S3 bucket."
+}
+
+variable lambda_s3_key {
+  description = "Lambda function S3 key."
+}
+
+variable posts_s3_bucket {
+  description = "Posts S3 bucket."
+}
+
+variable posts_s3_prefix {
+  description = "Posts S3 prefix."
+}
+
+variable slack_s3_bucket {
+  description = "Slack S3 bucket."
+}
+
+variable slack_s3_prefix {
+  description = "Slack S3 prefix."
+}
+
+variable twitter_access_token {
+  description = "Twitter API access token."
+}
+
+variable twitter_access_token_secret {
+  description = "Twitter API access token secret."
+}
+
+variable twitter_consumer_key {
+  description = "Twitter API Consumer Key."
+}
+
+variable twitter_consumer_secret {
+  description = "Twitter API Consumer Secret."
+}
+
+variable tags {
+  description = "Resource tags."
+  type        = map
+  default     = {}
+}
