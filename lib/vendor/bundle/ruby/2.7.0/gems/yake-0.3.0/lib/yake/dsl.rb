9e09f17d398022741645d02dfd94f5fa10a65117
Alexander Mancevice <alexander.mancevice@hey.com>
2021-07-15 18:50:05 -0400

    re-add vendor

---
+++ lib/vendor/bundle/ruby/2.7.0/gems/yake-0.3.0/lib/yake/dsl.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "json"
+
+require_relative "logger"
+
+module Yake
+  module DSL
+    ##
+    # Lambda handler task wrapper
+    def handler(name, &block)
+      define_method(name) do |event:nil, context:nil|
+        Yake.wrap(event, context, &block)
+      end
+    end
+
+    ##
+    # Helper to get logger
+    def logger
+      Yake.logger
+    end
+
+    ##
+    # Turn logging on/off
+    def logging(switch, logger = nil)
+      if switch == :on
+        Yake.logger = logger
+      elsif switch == :off
+        Yake.logger = ::Logger.new(nil)
+      else
+        raise Errors::UnknownLoggingSetting, switch
+      end
+    end
+  end
+end
+
+extend Yake::DSL
