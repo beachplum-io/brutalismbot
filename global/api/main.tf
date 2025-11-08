d491a8df089d3becd53762f33297f255bdc6b544
Alexander Mancevice <alexander.mancevice@hey.com>
2025-11-08 16:18:46 -0500

    aws ~> v6.0

--- global/api/main.tf
+++ global/api/main.tf
@@ -1,18 +1,3 @@
-#################
-#   TERRAFORM   #
-#################
-
-terraform {
-  required_version = "~> 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.2"
-    }
-  }
-}
-
 ############
 #   DATA   #
 ############
--- global/terraform.tf
+++ global/terraform.tf
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.2"
+      version = "~> 6.0"
     }
   }
 }
